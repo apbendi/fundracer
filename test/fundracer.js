4de57ce04603ba7fc58e7f8228f1431db985d991
Ben DiFrancesco <ben@scopelift.co>
2019-11-05 15:05:03 -0500

    Import token instance in tests

--- test/fundracer.js
+++ test/fundracer.js
@@ -1,21 +1,24 @@
 const FundRace = artifacts.require("FundRace");
 const devParams = require("../dev-params");
+const ERC20ABI = require("./helpers/erc20-abi");
 
 contract("FundRace", _accounts => {
 
     var instance = null;
+    var token = null
 
     before(async () => {
         instance = await FundRace.deployed();
+        token = new web3.eth.Contract(ERC20ABI, devParams.tokenAddr);
     });
 
     it("should deploy with the correct initalization parameters", async () => {
         let racer1 = await instance.racer1();
         let racer2 = await instance.racer2();
-        let token = await instance.donationToken();
+        let donationToken = await instance.donationToken();
 
         assert.equal(racer1, devParams.racer1, "Incorrect racer1 deployed");
         assert.equal(racer2, devParams.racer2, "Incorrect racer2 deployed");
-        assert.equal(token, devParams.tokenAddr, "Incorrect token deployed");
+        assert.equal(donationToken, token.options.address, "Incorrect token deployed");
     });
 });
