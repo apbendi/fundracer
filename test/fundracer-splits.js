12267e82320d8f070c75c1b9f11a61337deacf27
Ben DiFrancesco <ben@scopelift.co>
2020-02-10 16:54:27 -0500

    Unit Test zero take for zero designations

--- test/fundracer-splits.js
+++ test/fundracer-splits.js
@@ -1,8 +1,20 @@
-const generateParams = require("../dev-params")(undefined, "fundracer-splits.js");
+const FundRace = artifacts.require("FundRace");
 
-contract("FundRace Splits", () => {
+contract("FundRace Splits", _accounts => {
 
-    it("should run a test", async () => {
-        assert(true, "Logic is broken");
+    var instance = null;
+    let utils = web3.utils;
+
+    before(async () => {
+        instance = await FundRace.deployed();
+        assert(instance.address.startsWith("0x"), "Deployed Contract Not Found");
+    });
+
+    it("should return zeros when zero designations made", async () => {
+        let zero = utils.toWei("0", "ether");
+        let {racer1Take, racer2Take} = await instance.calculateSplits(zero, zero);
+
+        assert.equal(racer1Take.toString(10), "0", "Unexpected Racer1 Take");
+        assert.equal(racer2Take.toString(10), "0", "Unexpected Racer2 Take");
     });
 });
